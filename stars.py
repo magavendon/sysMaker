cb9a99a37dcbd2f5f32f7ed10a111f825a20a2fe
Magavendon <magkirby9000@gmail.com>
2017-09-25 17:42:43 -0500

    v0.2.2  Added calls to log and ran once for debugging.

--- stars.py
+++ stars.py
@@ -15,6 +15,7 @@ version = '1'
 # Default Python Imports
 from sys import exit as quit
 # Project File Imports
+from logger import log
 from menu import useMenu
 
 #----------------------------------------------------------------------------------------
@@ -25,17 +26,23 @@ from menu import useMenu
 # ██       ██████  ██   ████  ██████    ██    ██  ██████  ██   ████ ███████
 #----------------------------------------------------------------------------------------
 def getStars(random = True):
+    log('In Get Stars')
     # Determine the number of stars
     numberOfStars = amountOfStars(random)
     if numberOfStars == 0:
+        log('User chose an amount of stars')
         return []
 
+    log('User chose to Go Back')
     return ['he']
 
 def amountOfStars(random):
+    log('In Amount Of Stars')
     if random:
+        log('Passing on random')
         pass
     else:
+        log('Doing choice')
         choices = ['1 star',
                    '2 stars',
                    '3 stars',
@@ -43,10 +50,13 @@ def amountOfStars(random):
                    'Quit']
         selection = useMenu(choices)
         if selection == 3:
+            log('User chose to Go Back')
             return 0
         elif selection == 4:
+            log('User chose to Quit')
             quit()
 
+        log('User chose an amount of stars\nLeaving Amount of Stars')
         return selection
 
 #----------------------------------------------------------------------------------------
