f6591cfb9f94586993b7d6cd287707ec2d124b94
James Grider <james.grider@faa.gov>
2018-03-07 09:47:01 -0600

    Update roll. Add unit test.

--- roll.py
+++ roll.py
@@ -49,3 +49,44 @@ def rollDice(amount, dieType, showIndividualRolls = False):
     individualRolls = ', '.join(str(roll) for roll in rolls)
     return 'Rolls: {}\n  Sum: {}'.format(individualRolls, sumOfRolls)
   return sumOfRolls
+
+#----------------------------------------------------------------------------------------
+# ██    ██ ███    ██ ██ ████████     ████████ ███████ ███████ ████████
+# ██    ██ ████   ██ ██    ██           ██    ██      ██         ██
+# ██    ██ ██ ██  ██ ██    ██           ██    █████   ███████    ██
+# ██    ██ ██  ██ ██ ██    ██           ██    ██           ██    ██
+#  ██████  ██   ████ ██    ██           ██    ███████ ███████    ██
+#----------------------------------------------------------------------------------------
+if __name__ == '__main__':
+  roll = None
+  for x in range(0,1000):
+    roll = rollDie(6)
+  print(roll)
+
+  customFaces = ['hello', 'world', 'foo', 'bar', 'boo', 'baz']
+  for x in range(0, 1000):
+    roll = rollCustomDie(customFaces))
+  print(roll)
+
+  roll = [None, None, None, None]
+  for x in range(0, 4):
+    for y in range(0, 250):
+      roll[0] = rollDice(2, 4)
+    for y in range(0, 250):
+      roll[1] = rollDice(3, 4)
+    for y in range(0, 250):
+      roll[2] = rollDice(4, 4)
+    for y in range(0, 250):
+      roll[3] = rollDice(5, 4)
+  print(roll)
+
+  for x in range(0, 4):
+    for y in range(0, 250):
+      roll[0] = rollDice(2, 4, True)
+    for y in range(0, 250):
+      roll[1] = rollDice(3, 4, True)
+    for y in range(0, 250):
+      roll[2] = rollDice(4, 4, True)
+    for y in range(0, 250):
+      roll[3] = rollDice(5, 4, True)
+  print(roll)
