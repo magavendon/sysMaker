03e05059ef14f6984fcbceb6528a16319687eae9
Magavendon <magavendon@outlook.com>
2020-09-13 09:32:32 -0500

    Delete files for Qt rework

--- creator.py
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-#   Project:    sysMaker
-#   File:       creator.py
-#-------------------------------------------------------------------------------
-'''The main file for running the entire program.'''
-
-import yaml
-import logger
