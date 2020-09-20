01d23e6e6a9972f33f83335aed45e77641e5f225
Magavendon <magavendon@outlook.com>
2020-09-20 17:53:45 -0500

    Add line edits for star masses

--- steps/star_masses.py
+++ steps/star_masses.py
@@ -1,6 +1,9 @@
 from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QGroupBox
 from PySide2.QtWidgets import QHBoxLayout
 from PySide2.QtWidgets import QLabel
+from PySide2.QtWidgets import QLineEdit
+from PySide2.QtWidgets import QPushButton
 from PySide2.QtWidgets import QVBoxLayout
 
 from window import Window
@@ -15,7 +18,7 @@ class Star_Masses(Window):
         # Masses label
         self.masses = QLabel('A: None')
         self.masses.setFont(self.sys_font)
-        self.update_masses()
+        self.update_masses_label()
         masses_layout = QHBoxLayout()
         masses_layout.addWidget(self.masses)
         masses_layout.addStretch()
@@ -25,14 +28,69 @@ class Star_Masses(Window):
         description.setWordWrap(True)
         description.setAlignment(Qt.AlignJustify)
 
+        # Mass selection box
+        mass_selection = QGroupBox('Choose the star mass(es)')
+        mass_selection.setFont(self.box_title_font)
+        selection_layout = QHBoxLayout()
+        selection_layout.addWidget(mass_selection)
+        selection_layout.addStretch()
+
+        # First mass selection
+        first_label = QLabel('Star A:')
+        first_mass  = QLineEdit()
+        first_set   = QPushButton('Set Mass')
+        first_label.setFont(self.sys_font)
+        first_mass.setFont(self.sys_font)
+        first_set.setFont(self.sys_font)
+        first_layout = QHBoxLayout()
+        first_layout.addWidget(first_label)
+        first_layout.addWidget(first_mass)
+        first_layout.addWidget(first_set)
+        first_layout.addStretch()
+
+        # Second mass selection
+        second_label = QLabel('Star B:')
+        second_mass  = QLineEdit()
+        second_set   = QPushButton('Set Mass')
+        second_label.setFont(self.sys_font)
+        second_mass.setFont(self.sys_font)
+        second_set.setFont(self.sys_font)
+        second_layout = QHBoxLayout()
+        second_layout.addWidget(second_label)
+        second_layout.addWidget(second_mass)
+        second_layout.addWidget(second_set)
+        second_layout.addStretch()
+
+        # Third mass selection
+        third_label = QLabel('Star C:')
+        third_mass  = QLineEdit()
+        third_set   = QPushButton('Set Mass')
+        third_label.setFont(self.sys_font)
+        third_mass.setFont(self.sys_font)
+        third_set.setFont(self.sys_font)
+        third_layout = QHBoxLayout()
+        third_layout.addWidget(third_label)
+        third_layout.addWidget(third_mass)
+        third_layout.addWidget(third_set)
+        third_layout.addStretch()
+
+        # Set mass selection box layout
+        mass_selection_layout = QVBoxLayout()
+        mass_selection_layout.addLayout(first_layout)
+        mass_selection_layout.addLayout(second_layout)
+        mass_selection_layout.addLayout(third_layout)
+        mass_selection.setLayout(mass_selection_layout)
+
         # Create layout
         layout = QVBoxLayout()
         layout.addLayout(masses_layout)
         layout.addWidget(description)
+        layout.addSpacing(10)
+        layout.addLayout(selection_layout)
         layout.addStretch()
         self.setLayout(layout)
 
-    def update_masses(self):
+    def update_masses_label(self):
         if len(system.current.masses) > 0:
             update_text = ''
             for i in range(0, len(system.current.masses)):
@@ -43,4 +101,4 @@ class Star_Masses(Window):
             self.masses.setText(update_text)
 
     def update_info(self):
-        self.update_masses()
+        self.update_masses_label()
